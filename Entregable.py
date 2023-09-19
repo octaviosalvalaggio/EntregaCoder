ab58dc90516db1c84090cec7ca00869a53aa3f14
octavio <octaviosalvalaggio@gmail.com>
2023-09-19 12:39:32 -0300

    arreglo codigo

--- Entregable.py
+++ Entregable.py
@@ -24,40 +24,39 @@ symbols = ['BGN', 'BBD', 'EUR', 'BTN', 'AZN', 'KZT', 'KES', 'LYD', 'MDL', 'AMD']
 # Se llama a la función para obtener las tasas de cambio
 tasas_de_cambio = obtener_tasas_de_cambio(symbols)
 
-# Se verifica si las tasas de cambio se obtuvieron con éxito
-if tasas_de_cambio is not None:
-    print("Tasas de cambio para las criptomonedas:")
-    print(tasas_de_cambio)
-    
-    # Guardar los datos en un archivo JSON
-    with open('tasas_de_cambio.json', 'w') as archivo_json:
-        json.dump(tasas_de_cambio, archivo_json, indent=4)
-        print("Datos guardados en 'tasas_de_cambio.json'")
-
-# Credenciales desde el archivo .env
-username = config('DB_USERNAME')
-password = config('DB_PASSWORD')
-host = config('DB_HOST')
-port = config('DB_PORT')
-database_name = config('DB_NAME')
-
-# URL de conexión
-url = f'postgresql://{username}:{password}@{host}:{port}/{database_name}'
-
-# Conexión a la base de datos
-conn = create_engine(url)
-
 # Verificar si las tasas de cambio se obtuvieron con éxito antes de continuar
 if tasas_de_cambio is not None:
+    if "motd" in tasas_de_cambio:
+        del tasas_de_cambio["motd"]
+
     # Convertir los datos en un DataFrame de Pandas
     df = pd.DataFrame(tasas_de_cambio)
+
+    # Resetear el índice y renombrar la columna del índice
+    df.reset_index(inplace=True)
+    df.rename(columns={"index": "Enterprise"}, inplace=True)
+
+    # Imprimir el DataFrame
+    print(df)
     
-    # Renombrar las columnas del DataFrame para que coincidan con la tabla en DBeaver
-    df = df.rename(columns={'base': 'moneda_base', 'date': 'fecha', 'rates': 'tasas'})
-    
+    # Credenciales desde el archivo .env
+    username = config('DB_USERNAME')
+    password = config('DB_PASSWORD')
+    host = config('DB_HOST')
+    port = config('DB_PORT')
+    database_name = config('DB_NAME')
+
+    # URL de conexión
+    url = f'postgresql://{username}:{password}@{host}:{port}/{database_name}'
+
+    # Conexión a la base de datos
+    conn = create_engine(url)
+
     try:
         # Insertar los datos en la tabla 'tasas_de_cambio', evitando duplicados
         df.to_sql('tasas_de_cambio', conn, if_exists='append', index=False)
         print("Datos insertados en la tabla 'tasas_de_cambio'")
     except IntegrityError as e:
         print("Error al insertar datos en la tabla 'tasas_de_cambio':", e)
+else:
+    print("No se obtuvieron tasas de cambio.")

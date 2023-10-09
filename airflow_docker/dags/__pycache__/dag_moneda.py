d234473774ff1b42298f7784845edb622abf116c
octavio <octaviosalvalaggio@gmail.com>
2023-10-09 11:17:52 -0300

    Arreglo DAG

--- airflow_docker/dags/__pycache__/dag_moneda.py
+++ airflow_docker/dags/__pycache__/dag_moneda.py
@@ -1,6 +1,6 @@
 from datetime import datetime, timedelta
 from airflow import DAG
-from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
 
 default_args = {
     'owner': 'airflow',
@@ -12,22 +12,39 @@ default_args = {
     'retry_delay': timedelta(minutes=5),
 }
 
-
 dag = DAG(
     'crypto_api_dag',
     default_args=default_args,
     description='DAG to fetch and store cryptocurrency exchange rates',
-    schedule_interval=timedelta(days=1),  # Ajusta la frecuencia según tus necesidades
+    schedule_interval=timedelta(days=1),
 )
 
 # Comando bash que ejecutará tu script de Python
-bash_command = 'EntregaCoder.py'
+python_command = 'Entregable.py'
 
-# Crea la tarea utilizando el operador BashOperator
-run_etl_task = BashOperator(
+# Tarea para ejecutar el script ETL
+def execute_etl_function():
+    # Coloca aquí las operaciones ETL que deseas realizar
+    pass
+
+run_etl_task = PythonOperator(
     task_id='run_etl',
-    bash_command=bash_command,
+    python_callable=execute_etl_function,
     dag=dag,
 )
+
+# Tarea para limpiar después de la ejecución del script ETL
+def clean_up_function():
+    # Coloca aquí las operaciones de limpieza que deseas realizar
+    pass
+
+clean_up_task = PythonOperator(
+    task_id='clean_up',
+    python_callable=clean_up_function,
+    dag=dag,
+)
+
+# Define el orden de ejecución de las tareas
+run_etl_task >> clean_up_task
 
 run_etl_task
